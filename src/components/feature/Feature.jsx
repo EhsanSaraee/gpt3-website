3ea12c1806200d7522dff023a7c65df20d8bf1e3
ehsan <ehsansaraee6570@yahoo.com>
2022-03-17 19:59:44 +0330

    adding feature component & styles

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,15 @@
-const Feature = () => {
-  return ( 
-    <>Feature</>
+const Feature = ({ title, text }) => {
+   return (
+      <section className="gpt3__features-container__feature">
+         <div className="gpt3__features-container__feature-title">
+            <div />
+            <h1>{title}</h1>
+         </div>
+         <div className="gpt3__features-container__feature-text">
+            <p>{text}</p>
+         </div>
+      </section>
    );
-}
- 
+};
+
 export default Feature;
